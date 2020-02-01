2210387150fa08876c93ac1c928e62bb0f909aba
Jose Martinez <turegano.jose@gmail.com>
2020-02-01 12:36:43 +0000

    Added extra comments as specified

--- GravitySimulation.py
+++ GravitySimulation.py
@@ -4,113 +4,149 @@ import matplotlib.pyplot as plt  # need plots
 # By default all units are SI
 # Positive -> Upward (gravity always negative)
 
-# Main constants
+# Simulation constants
 simulation_time = 10  # [s]
 PI = 3.141592
 G = 6.67408e-11  # Gravitational constant[m3 kg-1 s-2]
 dt = 0.001  # step time[s]
 
 
+# Classes definitions
+
 class Fluid:
-   def __init__(self, density):
-       self.density = density
+    def __init__(self, density):
+        self.density = density  # [kg m-3]
 
 
 class Planet:
-   def __init__(self, radius, mass, gravity_acc):
-       self.radius = radius
-       self.mass = mass
-       self.g = gravity_acc
+    def __init__(self, radius, mass, gravity_acc):
+        self.radius = radius  # [m]
+        self.mass = mass      # [kg]
+        self.g = gravity_acc  # [m s-2]
 
 
 class Object:
-   def __init__(self, mass, drag_coeff, radius, state, fluid, planet):
-       if mass < 0.000001:
-           print('Warning: very small or close to zero mass')
-       self._mass = mass
-       self._drag_coeff = drag_coeff
-       self._radius = radius
-       self._state = state  # 0) Position 1) Velocity
-       # self.__derivative = derivative  # 0) Velocity 1) Acceleration
-       self._fluid = fluid
-       self._planet = planet
+    def __init__(self, mass, drag_coeff, radius, state, fluid, planet):
+        if mass < 0.000001:
+            print('Warning: very small or close to zero mass')
+        self._mass = mass               # [kg]
+        self._drag_coeff = drag_coeff   # Drag coefficient []
+        self._radius = radius           # [m]
+        self._state = state             # State vector: 0) Position 1) Velocity
+        self._fluid = fluid             # Fluid class
+        self._planet = planet           # Planet class
 
-   def drag_force(self, speed):
-       return - np.sign(speed) * self._drag_coeff * PI * self._radius * self._radius * self._fluid.density * speed * speed
+    """
+    drag_force()
+    Input: obejct speed [m s-1]
+    Output: drag force [N]
+    """
+    def drag_force(self, v_object):
+        return - np.sign(
+            v_object) * self._drag_coeff * PI * self._radius * self._radius * self._fluid.density * v_object * v_object
+    """
+    gravity()
+    This is a polymorphic function. If input is defined, newtonian gravity is applied.
+    If there is not input to this function, constant gravity is applied
+    No fallback strategy if height_pos is negative or equal to the planet radius
+    Input: height position of the object [m]
+    Output: gravity pulling force [N]
+    """
+    def gravity(self):
+        return - self._planet.g * self._mass
 
-   def gravity(self):
-       return - self._planet.g * self._mass
-   def gravity(self, height_pos):
-       return -G * self._planet.mass * self._mass/((self._planet.radius+height_pos)*(self._planet.radius+height_pos))
-   # WATCH OUT FOR DIVIDE BY ZERO
+    def gravity(self, height_pos):
+        return -G * self._planet.mass * self._mass / (
+                    (self._planet.radius + height_pos) * (self._planet.radius + height_pos))
 
-   def calculate_acceleration(self, input_state):
-       # Equation of motion
-       return np.array([input_state[1], (self.drag_force( input_state[1]) + self.gravity(input_state[0])) / self._mass])  # 0) Velocity 1) Acceleration
+    """""
+    calculate_acceleration()
+    Equation of motion Drag - Weight = object mass multiplied by acc
+    If argument of gravity function is removed, a constant gravity
+    model is applied, instead of newtonian gravity.
+    Returns derivative of state vector: 0) Velocity 1) Acceleration
+    Input: vector state: 0) Position [m] 1) Velocity [m s-1]
+    Output: [Velocity, acceleration] [m s-1] [m s-2]
+    """
+    def calculate_acceleration(self, input_state):
+        return np.array([input_state[1], (self.drag_force(input_state[1]) + self.gravity(
+            input_state[0])) / self._mass])  # 0) Velocity 1) Acceleration
 
-   def first_derivative(self):
-       derivative_state = self.calculate_acceleration(self._state)
-       return derivative_state
+    def first_derivative(self):
+        derivative_state = self.calculate_acceleration(self._state)
+        return derivative_state
 
-   def next_derivative(self, derivative, h):
-       # temp_state = np.array([0.0, 0.0])  # temp state vector: 0) Position 1) Velocity
-       temp_state = self._state + derivative * h
-       return self.calculate_acceleration(temp_state)
+    def next_derivative(self, derivative, h):
+        # temp_state = np.array([0.0, 0.0])  # temp state vector: 0) Position 1) Velocity
+        temp_state = self._state + derivative * h
+        return self.calculate_acceleration(temp_state)
+    """""
+    integrator_rk4()
+    Updates state vector using RK4
+    """""
+    def integrator_rk4(self):
+        k1 = self.first_derivative()
+        k2 = self.next_derivative(k1, dt * 0.5)
+        k3 = self.next_derivative(k2, dt * 0.5)
+        k4 = self.next_derivative(k3, dt)
+        self._state = self._state + dt * (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+    """""
+    integrator_rk4()
+    Alternatively updates state vector using Euler
+    """""
+    def integrator_euler(self):
+        self._state = self._state + dt * self.calculate_acceleration(self._state)
 
-   def integrator_rk4(self):
-       k1 = self.first_derivative()
-       k2 = self.next_derivative( k1, dt * 0.5)
-       k3 = self.next_derivative( k2, dt * 0.5)
-       k4 = self.next_derivative( k3, dt)
-       self._state = self._state + dt* (1.0 / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
 
-   def integrator_euler(self):
-       self._state = self._state + dt * self.calculate_acceleration(self._state)
+"""""
+Main starts here
+"""""
 
-
-# Inputs
-drop_height = 100.0 # [m]
-initial_velocity = 10.0 # [m s-1] Positive -> Upward
+# Initial object states
+drop_height = 100.0  # [m]
+initial_velocity = 10.0  # [m s-1] Positive -> Upward
+# Initialise classes
 air = Fluid(density=1.21)
 earth = Planet(radius=6371e3, mass=5.972e24, gravity_acc=9.81)
-ball = Object(mass=2, drag_coeff=1.0, radius=0.1, state=np.array([drop_height, initial_velocity]), fluid=air, planet=earth)
-
+ball = Object(mass=2, drag_coeff=1.0, radius=0.1, state=np.array([drop_height, initial_velocity]), fluid=air,
+              planet=earth)
 # logging variables
 position = []
 velocity = []
-acceleration =[]
+acceleration = []
 t = []
-position_check=[]
-velocity_check=[]
-acceleration_check=[]
+position_check = []
+velocity_check = []
+acceleration_check = []
 
 # Main loop
 time = 0
 while time < simulation_time:
-   # Update time step
-   ball.integrator_rk4()
+    # Update time step
+    ball.integrator_rk4()
 
-   # logging
-   position.append(ball._state[0])
-   velocity.append(ball._state[1])
-   [ speed, acc] = ball.calculate_acceleration(ball._state)
-   acceleration.append(acc)
+    # logging
+    position.append(ball._state[0])
+    velocity.append(ball._state[1])
+    [speed, acc] = ball.calculate_acceleration(ball._state)
+    acceleration.append(acc)
 
-   # logging check physics (no drag)
-   position_check.append(drop_height - earth.g * time*time/2)
-   velocity_check.append( -np.sqrt((ball._planet.g * ball._mass)/(ball._drag_coeff * PI * ball._radius * ball._radius * ball._fluid.density))) # terminal speed as reference
-   acceleration_check.append(-earth.g)
-   t.append(time)
-   time = time + dt
+    # logging check physics as a reference
+    position_check.append(drop_height - earth.g * time * time / 2) # No drag
+    velocity_check.append(-np.sqrt((ball._planet.g * ball._mass) / (
+                ball._drag_coeff * PI * ball._radius * ball._radius * ball._fluid.density)))  # terminal speed as reference
+    acceleration_check.append(-earth.g) # No drag
+    t.append(time)
+    time = time + dt
 
 # Plotting
 f, (ax1, ax2, ax3) = plt.subplots(3, sharex=True)
-ax1.plot(t,position, 'r', label='Constant Gravity')
+ax1.plot(t, position, 'r', label='Gravity')
 ax1.plot(t, position_check, 'k', label='No drag')
-ax2.plot(t,velocity, 'r', label='Constant Gravity')
-ax2.plot(t,velocity_check, 'k', label='Terminal velocity (Drag)')
-ax3.plot(t,acceleration, 'r', label='Constant Gravity')
-ax3.plot(t,acceleration_check, 'k', label='No drag')
+ax2.plot(t, velocity, 'r', label='Gravity')
+ax2.plot(t, velocity_check, 'k', label='Terminal velocity (Drag)')
+ax3.plot(t, acceleration, 'r', label='Gravity')
+ax3.plot(t, acceleration_check, 'k', label='No drag')
 
 ax1.set(ylabel='Position [m]')
 ax2.set(ylabel='Speed [m/s]')
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Coding challenge
 ## Gravitation sim
 Simple simulation of an object falling.
-I tried to be as **pythonic**.
+I tried to be as **pythonic** as possible.
 My code doesn't lean to the right. Still very long lines.
